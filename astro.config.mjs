6cc6d153428953a768b0d444c116d5eca1502dc5
Ellyn Kim <ehtk100109@gmail.com>
2026-07-15 14:40:53 -0400

    new config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ehtk100109-dot.github.io",
+  base: "/ekwebsite/",
   vite: {
     plugins: [tailwindcss()],
   },
